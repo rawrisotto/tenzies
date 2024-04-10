a469b88c9ea7563adcc1fc7c34212df9659b51b4
rawrisotto <kudahidayat8@gmail.com>
2024-04-10 13:11:03 +0800

    feat: Added board, dice and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import Die from "./components/Die"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [dice, setDice] = useState(generateInitialDice())
+
+  function getRandomValue() {
+    return Math.ceil(Math.random() * 6)
+  }
+
+  function generateInitialDice() {
+    const diceArray = []
+    
+    for (let i = 0; i < 10; i++) {
+      diceArray.push({
+        id: i + 1,
+        currentValue: getRandomValue(),
+        freeze: false,
+        roll: getRandomValue
+      })
+    }
+
+    return diceArray
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+    <main>
+      <section className="board">
+        <h1 className="board__title">Tenzies</h1>
+        <p className="board__rules">
+          Roll until all dice are the same. Click each die to freeze it at its
+          current value between rolls.
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+        <div className="die-wrapper">
+          <Die />
+          <Die />
+          <Die />
+          <Die />
+          <Die />
+          <Die />
+          <Die />
+          <Die />
+          <Die />
+          <Die />
+        </div>
+        <button className="btn">Roll</button>
+      </section>
+    </main>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Die.jsx
@@ -0,0 +1,9 @@
+const Die = () => {
+  return (
+    <div className="die">
+      <p className="die__number">1</p>
+    </div>
+  )
+}
+
+export default Die
